b0d9caf8e709db0c3ac1fdc1f9b559ad8e60c23c
Alena Nidilko <alenkan96@gmail.com>
2022-02-09 16:40:07 +0300

    Putt things order

--- client.py
+++ client.py
@@ -1,23 +1,30 @@
 import socket, uuid, random, string
 
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создание сокета
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 8000
-id_client = str(uuid.uuid3(uuid.NAMESPACE_DNS, 'python.org'))
-client.connect((IP, PORT))  # устанавливаем данные для прослушивания данного порта
-client.send(id_client.encode('utf8'))
-key = client.recv(1024)  # получаем ключ
-print('key = ', key.decode('utf8'))
-
 PORT1 = 8001
-client1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client1.connect((IP, PORT1))
-symbols = string.punctuation + string.ascii_uppercase + string.ascii_lowercase + string.digits
-message = ''.join(random.sample(symbols, random.randint(10, 30)))  # генерируем рандомное текстовое сообщение
-print('message = ', message)
-client1.send(message.encode('utf8'))
-client1.send(id_client.encode('utf8'))
-client1.send(key)
-result = client1.recv(1024)
-print(result.decode('utf8'))
+
+
+def send_id_and_rec_key():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создание сокета
+    client.connect((IP, PORT))  # устанавливаем данные для прослушивания данного порта
+    client.send(id_client.encode('utf8'))
+    key = client.recv(1024)  # получаем ключ
+    return key
+
+
+def send_message(key):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((IP, PORT1))
+    symbols = string.punctuation + string.ascii_uppercase + string.ascii_lowercase + string.digits
+    message = ''.join(random.sample(symbols, random.randint(10, 30)))  # генерируем рандомное текстовое сообщение
+    print('message = ', message)
+    client.send(message.encode('utf8'))
+    client.send(id_client.encode('utf8'))
+    client.send(key)
+
+
+if __name__ == '__main__':
+    id_client = str(uuid.uuid4())
+    k = send_id_and_rec_key()
+    send_message(k)
--- server.py
+++ server.py
@@ -4,36 +4,41 @@ import socket, random, string, logging
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 8000
 PORT1 = 8001
-client = {}  # Массив для хранения адресов клиентов
-logging.basicConfig(filename="sample.log", level=logging.INFO)
+CLIENT = {}  # Массив для хранения адресов клиентов
 
-while True:
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # создание сокета
-    server.bind((IP, PORT))  # устанавливаем данные для прослушивания данного порта
-    server.listen(50)  # разрешаем серверу принимать запросы
-    user, address = server.accept()  # в случае найденного пользователя разрешаем ему подключиться
-    symbols = string.digits + string.ascii_uppercase + string.ascii_lowercase
-    data = user.recv(1024).decode('utf8')  # получить 1024 байт данных
-    if data not in client.keys():
-        key = ''.join(random.sample(symbols, 8))  # сгенерировать код новому клиенту
-        client.update({data: key})  # Если такого клиента нету , то добавить его и сгенерированный код
-        print(data, '    => ', key)
-        user.send(key.encode('utf8'))
 
-    server1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server1.bind((IP, PORT1))
-    server1.listen(50)  # разрешаем серверу принимать запросы
-    user1, address1 = server1.accept()
-    message = user1.recv(1024).decode('utf8')  # получить 1024 байт данных
-    id_client = user1.recv(1024).decode('utf8')
-    key_client = user1.recv(1024).decode('utf8')
-    if id_client in client.keys():
-        if client[data] == key_client:
-            logging.info(message)
-            user1.send('ok'.encode('utf8'))
+def generate_and_send_key_for_new_client():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:  # создание сокета
+        server.bind((IP, PORT))  # устанавливаем данные для прослушивания данного порта
+        server.listen(50)  # разрешаем серверу принимать запросы
+        user, address = server.accept()  # в случае найденного пользователя разрешаем ему подключиться
+        symbols = string.digits + string.ascii_uppercase + string.ascii_lowercase
+        data = user.recv(1024).decode('utf8')  # получить 1024 байт данных
+        if data not in CLIENT.keys():
+            key = ''.join(random.sample(symbols, 8))  # сгенерировать код новому клиенту
+            CLIENT.update({data: key})  # Если такого клиента нету , то добавить его и сгенерированный код
+            user.send(key.encode('utf8'))
+
+
+def check_client_and_write_message_in_log():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.bind((IP, PORT1))
+        server.listen(50)  # разрешаем серверу принимать запросы
+        user, address = server.accept()
+        message = user.recv(1024).decode('utf8')  # получить 1024 байт данных
+        id_client = user.recv(1024).decode('utf8')
+        key_client = user.recv(1024).decode('utf8')
+        if id_client in CLIENT.keys():
+            if CLIENT[id_client] == key_client:
+                logging.info(message)
+            else:
+                logging.error("Wrong key")
         else:
-            logging.error("Wrong key")
-            user1.send('Wrong key'.encode('utf8'))
-    else:
-        logging.error("Get the key first")
-        user1.send('Get the key first'.encode('utf8'))
+            logging.error("Get the key first")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename="sample.log", level=logging.INFO)
+    generate_and_send_key_for_new_client()
+    check_client_and_write_message_in_log()
+
